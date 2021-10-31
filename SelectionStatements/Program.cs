5612d69f0aa7701190c2634aa67501de7e68aa34
Frederick Bradley <fbradley21@icloud.com>
2021-10-31 15:33:09 -0500

    completed Selection Statement exercise

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess my favorite number!");
+            Console.WriteLine("What is the upper limit the range of the number?");
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+            var random = new Random();
+            var favNumber = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if(guess > favNumber)
+            {
+                Console.WriteLine("Your guess is too high! Sorry");
+            }
+
+            else if (guess < favNumber)
+            {
+                Console.WriteLine("Your guess is too low! Sorry");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!/n");
+
+
+            }
+        }
+    }
+}
